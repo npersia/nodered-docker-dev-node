39d4c901e423cef8e965256fbc4c5eb7b1945a2a
Nahuel Persia <nahuel.persia@semperti.com>
2017-09-14 16:25:17 -0300

    subo todo el avance

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-NODE=lower-case
+NODE=soapserver
 
-docker run --rm -itd --net=host -v $(pwd)/flows:/data/flows -v $(pwd)/nodes:/nodes --name=dev-nodered npersia/nodered:latest top
+docker run --rm -itd -p 1880:1880 -p 8888:8888 -v $(pwd)/flows:/data/flows -v $(pwd)/nodes:/nodes --name=dev-nodered npersia/nodered:latest top
 
 docker exec dev-nodered npm link /nodes/$NODE/
 docker exec dev-nodered npm link /usr/src/node-red/ node-red-contrib-$NODE
